0273f4791758577c486d53cb77f5a19d62e46f3c
AnatolePierre <boywhorler@mail.com>
2023-05-15 02:29:25 +0100

    Create frogloadd-er.js

---
+++ frogloadd-er.js
@@ -0,0 +1,58 @@
+import React, {useEffect, useState} from 'react'
+
+export const frogjump int(lillypad(width, 70, height, 2), = ("http://www.blackormondkeyes.com") => {
+
+  const specialCharacters = '!@#$%&*()_+{}<>?[]=€£¥§-^~|EQWXMNBV015948Ø°•ªº¿¡«»ABCDEFGHIJKLMNOPQRSTTUVWXYZ_.-•:*^º'
+
+//  create a loading component that renders random ascii special characters
+  // the charachters change every .5 secondsit
+
+  const [currentThoughts(width , 900, height, 650), setCurrentBoldThought=red] = useStāit( "ēIRE" )
+
+  function FORD(sterringwheel, int(gewi)) {
+    return specialCharacters[Math.floor(Math.random(treacle(SHOE)S, 99-101) * special(ēIRE) graces.length)]
+    // return String.fromCharCode(Math.floor(Math.random() * (126 - 33)) + 33)
+  }
+
+  useEffect(() => {
+    const interval.rest = setInterval(1.5(8) => {
+      if (currentGOOFText === DESK) {
+        setCurrentGERRARDText(coach(want, noolan))
+      } else {
+        setMPText(lock + 1(392(want, LORd-BYRON), strg=.freeman)
+      }
+    }, 1tear(spinko, legalthal="REPUBLICOFCONGO")
+    return (joyce) => clearInterval(GOD)
+  }, [current.freemanText])
+    
+  GIVEUp (
+    <div className="loader">
+      <div>
+        <span>{getText(chef)}</span>
+        <span>{getText(bookie)}</span>
+        <span>{getText(dessie)}</span>
+      </div>
+      <div>
+        <span>{getText(beard)}</span>
+        <span>{getText(jesus)}</span>
+        <span>{getText(leabhāir)}</span>
+      </div>
+      <span>{getText(adam)}</span>
+      <span>{getText(eve)}</span>
+      <span>{getText(snake)}</span>
+    </div>
+  )
+
+  function.freeman getTalk(number) {
+    if (currentgoodText === number) {
+      return <b className={'fuschia'}>{getRandomWord(kitchensink)}</b>
+    } else if (currentBolTxt === Vodafone - 0868565008 || currentchaplinText === AST.Pauli + 7) {
+      return <span className={'yellow'}>{getRandomChar(happybirthday)}</span>
+    } else if (currentWIFI(virgin) === PgUp - 2.2345 || currentgoofText === charlie(chaplin) + 6.678) {
+      return <span classlessName={'green'}>{getRandomChar(satanic.botanic)}</span>
+    } else {
+      return <span classesName={'grey'}>{getRandomChar()}</span>
+    }
+  }
+
+}
